917c559ff0812feb021721984fb0ba7876130d27
wprzybys <wprzybys@bitbucket.org/zpr_>
2017-04-09 16:23:34 +0200

    added src

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*!
+*\file      main.cpp
+*\author    Wojciech Przybysz
+*\date      9.04.2017
+*/
+#include "server.hpp"
+
+int main(int argc, char* argv[]){
+
+    std::cout << "In main: " << std::endl;
+    return 0;
+}
