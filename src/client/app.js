c8f3544414eb8ca3bce96520d4bd097776290419
Kajetan <kajetanspionek@gmail.com>
2017-05-20 19:20:21 +0200

    Color as parameter of player's ball

--- src/client/app.js
+++ src/client/app.js
@@ -4,26 +4,27 @@ class Food {
     this.x_ = x;
     this.y_ = y;
     this.id_ = id;
-    this.r_ = 6;
+    this.r_ = 6; 
   }
 
   show() {
-      drawCircle(this.x_, this.y_,7);
+      drawCircle(this.x_, this.y_,7, 0);
   }
 
 }
 
 class Player {
-  constructor(id,x,y,r) {
+  constructor(id,x,y,r,color) {
 
     this.x_ = x;
     this.y_ = y;
     this.id_ = id;
     this.r_ = r;
+    this.color_ = color;
   }
 
   show() {
-      drawCircle(this.x_, this.y_,this.r_);
+      drawCircle(this.x_, this.y_,this.r_,this.color_);
   }
 
   update() {
