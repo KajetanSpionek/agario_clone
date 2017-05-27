f5e49e2b997aa6ec5430557efeefbb9cc5e225a0
Wojciech Przybysz <przybysz_w@outlook.com>
2017-05-27 20:29:30 +0000

    element.cpp edited online with Bitbucket

--- src/server/element.cpp
+++ src/server/element.cpp
@@ -1,7 +1,7 @@
 #include "element.hpp"
 
 namespace websocket {
-	std::atomic<int> Element::idCount_;
+	
 
 	Element::Element(int & x,int & y,int & radius): id_(++idCount_), x_(x), y_(y),radius_(radius)
 	{
