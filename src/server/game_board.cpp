a0aa218f8c8503c41e3c341b07b25560349418c8
wprzybys <wprzybys@bitbucket.org/zpr_>
2017-06-01 20:30:47 +0200

    bug sigsegv elements_.getId() solved

--- src/server/game_board.cpp
+++ src/server/game_board.cpp
@@ -136,6 +136,8 @@ namespace websocket {
             id = temp_food->getId();
 
             auto new_food = elements_.insert(std::make_pair(id,temp_food));
+
+            std::cout << "map elements_.size():  " << elements_.size() << std::endl;
             
             tmp_foods.insert((*new_food.first));
 
@@ -334,6 +336,10 @@ namespace websocket {
 
         balls_container::iterator j;
 
+        std::cout << "gameState map elements_.size():  " << elements_.size() << std::endl;
+        //if (!elements_.empty())
+              //  std::cout << "map elements_ size:" << std::cout; 
+
         for(j = balls_.begin(); j != balls_.end(); j++ )
         {
             header_balls = header_balls + " " + boost::lexical_cast<std::string>((j->second)->getId());
@@ -467,7 +473,7 @@ namespace websocket {
                     }
                     else
                     {
-                        auto it = elements_[i];
+                       // auto it = elements_.at(i);
                     }
                 }
             }
