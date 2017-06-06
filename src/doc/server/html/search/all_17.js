c0b76393c5a58057f0741d26730bad74a41b8508
wprzybys <wprzybys@bitbucket.org/zpr_>
2017-06-06 14:48:59 +0200

    doc changed

--- src/doc/server/html/search/all_17.js
+++ src/doc/server/html/search/all_17.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['_7eball',['~Ball',['../classwebsocket_1_1Ball.html#abf56827e2e9042b2b0eaa71fed17b725',1,'websocket::Ball']]],
-  ['_7eelement',['~Element',['../classwebsocket_1_1Element.html#a4a249effc753deac10eb2eb7fc80c759',1,'websocket::Element']]],
-  ['_7efooditem',['~FoodItem',['../classwebsocket_1_1FoodItem.html#a870abf88e3f80d5218fa017e52601d60',1,'websocket::FoodItem']]],
+  ['_7eball',['~Ball',['../classwebsocket_1_1Ball.html#a07e8b6854de320eeb0def600daece544',1,'websocket::Ball']]],
+  ['_7eelement',['~Element',['../classwebsocket_1_1Element.html#a6d08a32162fcbc445a491259598f465f',1,'websocket::Element']]],
+  ['_7efooditem',['~FoodItem',['../classwebsocket_1_1FoodItem.html#a1d35296b7eca7b0ed40f0ab1dc3248d8',1,'websocket::FoodItem']]],
   ['_7eplayer',['~Player',['../classwebsocket_1_1Player.html#a3471cfad0e035a4204ff3f7e83a239f8',1,'websocket::Player']]]
 ];
